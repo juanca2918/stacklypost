d61a0c50b703c71af967bae44c3a74ac31f6ed45
Juan Carlos <jacal2211@gmail.com>
2021-04-07 12:42:39 -0500

    Se crea el repo de stacklypost

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>StacklyPost</title>
+</head>
+<body>
+    <div class="contenedor-inicio">
+
+        <header>
+            <?php include("vista/navbar/navbar.php"); ?>
+        </header>
+
+        <div class="grid-inicio">
+            <!--INICIO CARD POST-->
+            <div class="componente-card">
+                <?php include("vista/card/card.php"); ?>
+            </div>
+            <!--FIN CARD POST-->
+        </div>
+    </div>
+    <footer>
+        <?php include("vista/footer/footer.php"); ?>
+    </footer>
+</body>
+</html>
---
+++ vista/footer/footer.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Footer</title>
+</head>
+<body>
+    <div class="contenedor-footer">
+        <div class="grid-footer">
+
+            <div class="footer-item">
+                <p>StacklyCode ® Marca Registrada</p>
+            </div>
+
+            <div class="footer-item">
+                <p>StacklyCode@</p>
+            </div>
+
+            <div class="footer-item">
+                <p>Redes Sociales</p>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
